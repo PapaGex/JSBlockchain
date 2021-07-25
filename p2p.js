e636d21c20260d5c7375fed4266c36ab5bac8a30
PapaGex <cryptoblockchainjunkie@gmail.com>
2021-07-25 17:09:04 -0400

    initial commit

---
+++ p2p.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+const Swarm = require('discovery-swarm');
+const defaults = require('dat-swarm-defaults');
+const getPort = require('get-port');
+
+const peers = {};
+let connSeq = 0;
+let channel = 'bloodlines';
+
+const myPeerId = crypto.randomBytes(32);
+console.log(`myPeerId: ${myPeerId.toString('hex')}`);
+
+const config = defaults({
+    id: myPeerId,
+});
+
+const swarm = Swarm(config);
+
+(async () => {
+    const port = await getPort();
+
+    swarm.listen(port);
+})
